e0a3a4f04e667722cc9db71ea8ed75c40c0efa48
Adam Malcontenti-Wilson <amalcontenti-wilson@zendesk.com>
2014-03-14 11:45:57 +1100

    Add CORS support for static assets

--- lib/zendesk_apps_tools/server.rb
+++ lib/zendesk_apps_tools/server.rb
@@ -1,13 +1,22 @@
 require 'sinatra/base'
+require 'sinatra/cross_origin'
 require 'zendesk_apps_support/package'
 
 module ZendeskAppsTools
   class Server < Sinatra::Base
+    register Sinatra::CrossOrigin
+
     set :public_folder, Proc.new {"#{settings.root}/assets"}
+    set :allow_origin, :any
 
     get '/app.js' do
       content_type 'text/javascript'
       ZendeskAppsSupport::Package.new(settings.root).readified_js(nil, 0, "http://localhost:#{settings.port}/", settings.parameters)
+    end
+
+    def static!
+      cross_origin
+      super
     end
 
   end
